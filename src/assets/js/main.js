1c852e204365f6cf19a268996b00dfb8f99a38a4
Zzal1225 <pakcam35@naver.com>
2023-08-11 14:56:32 +0900

    비쥬얼영역 완료

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,9 +3,57 @@ import { port } from "./port.js";
 import { link } from "./link.js";
 import { smooth } from "./smooth.js";
 
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
 window.addEventListener("load", function () {
   menu();
   port();
   link();
   smooth();
 });
+
+const bgm = document.getElementById("bgm");
+const playButton = document.getElementById("playButton");
+const pauseButton = document.getElementById("pauseButton");
+
+playButton.addEventListener("click", () => {
+  bgm.play();
+});
+
+pauseButton.addEventListener("click", () => {
+  bgm.pause();
+});
+
+  gsap.registerPlugin(ScrollTrigger);
+
+
+  gsap.from("#img1",{
+    scrollTrigger:{
+      trigger: "#visual",
+      scrub:true,
+      start: "top center",
+      end: "bottom center",
+    },
+    y: -550,
+  })
+
+  gsap.from("#img4",{
+    scrollTrigger:{
+      trigger: "#visual",
+      scrub:true,
+      start: "top center",
+      end: "bottom center",
+    },
+    x: 650,
+  })
+
+  gsap.from("#img5",{
+    scrollTrigger:{
+      trigger: "#visual",
+      scrub: true,
+      start: "top right",
+      end: "bottom center",
+    },
+    x: -650,
+  })
