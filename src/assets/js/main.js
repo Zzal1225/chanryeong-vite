6f0652073e03634c7091f7b60e6222f929eb646a
Zzal1225 <pakcam35@naver.com>
2023-08-11 22:20:35 +0900

    intro 완료

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -25,35 +25,75 @@ pauseButton.addEventListener("click", () => {
   bgm.pause();
 });
 
-  gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
+gsap.from("#img1", {
+  scrollTrigger: {
+    trigger: "#visual",
+    scrub: true,
+    start: "top center",
+    end: "bottom center",
+  },
+  y: -550,
+});
 
-  gsap.from("#img1",{
-    scrollTrigger:{
-      trigger: "#visual",
-      scrub:true,
-      start: "top center",
-      end: "bottom center",
+gsap.from("#img4", {
+  scrollTrigger: {
+    trigger: "#visual",
+    scrub: true,
+    start: "top center",
+    end: "bottom center",
+  },
+  x: 650,
+});
+
+gsap.from("#img5", {
+  scrollTrigger: {
+    trigger: "#visual",
+    scrub: true,
+    start: "top right",
+    end: "bottom center",
+  },
+  x: -650,
+});
+
+const targets = gsap.utils.toArray(".split");
+
+targets.forEach((target) => {
+  let SplitClient = new SplitType(target, { type: "lines, words, chars" });
+  let lines = SplitClient.lines;
+  let words = SplitClient.words;
+  let chars = SplitClient.chars;
+
+  gsap.from(chars, {
+    yPercent: 100,
+    autoAlpha: 0,
+    duration: 1,
+    ease: "circ.out",
+    stagger: {
+      amount: 1,
+      from: "random",
     },
-    y: -550,
-  })
-
-  gsap.from("#img4",{
-    scrollTrigger:{
-      trigger: "#visual",
-      scrub:true,
-      start: "top center",
-      end: "bottom center",
+    scrollTrigger: {
+      trigger: target,
+      start: "top bottom",
+      end: "+=400",
+      markers: false,
     },
-    x: 650,
-  })
+  });
+});
 
-  gsap.from("#img5",{
-    scrollTrigger:{
-      trigger: "#visual",
-      scrub: true,
-      start: "top right",
-      end: "bottom center",
-    },
-    x: -650,
-  })
+gsap.utils.toArray(".parallax__item__desc").forEach(item => {
+  gsap.to(item, {
+      yPercent: -100,
+      ease: "none",
+      duration: 0.5,
+      scrollTrigger: {
+          trigger: item,
+          start: "top bottom", 
+          end: "bottom top",
+          scrub: 0.5
+      },  
+  });
+});
+
--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -1,4 +1,5 @@
+
+
 export function port() {
     console.log(port);
   }
-  
